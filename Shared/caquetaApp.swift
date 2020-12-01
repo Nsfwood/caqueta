8bc669b49dd9e778af6734bd11cdcaeb47b0e4e7
Alexander Rohrig <me@alexanderrohrig.com>
2020-12-01 07:43:42 -0800

    Initial Commit

---
+++ Shared/caquetaApp.swift
@@ -0,0 +1,17 @@
+//
+//  caquetaApp.swift
+//  Shared
+//
+//  Created by Alexander Rohrig on 12/1/20.
+//
+
+import SwiftUI
+
+@main
+struct caquetaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
